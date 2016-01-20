2e5498c5dbea438b96ef8faad1c75685cea45da7
Fernando Mayer <fernandomayer@gmail.com>
2016-01-20 21:04:39 -0200

    Add functions to copy and cut current line without selection

--- private/funk/funcs.el
+++ private/funk/funcs.el
@@ -114,7 +114,7 @@
 ;; key binding
 (global-set-key [?\C--] 'insert-rule-and-comment-3)
 
-;; To revert-buffer without confirmation
+;; To revert-buffer without confirmation ===============================
 ;; Source: http://www.emacswiki.org/emacs-en/download/misc-cmds.el
 (defun revert-buffer-no-confirm ()
   "Revert buffer without confirmation."
@@ -122,3 +122,35 @@
   (revert-buffer t t))
 ;; key binding
 (global-set-key [f5] 'revert-buffer-no-confirm)
+
+;; COPY the current line, whithout selecting it ========================
+;; Source: http://www.emacswiki.org/emacs/CopyingWholeLines
+(defun quick-copy-line ()
+  "Copy the whole line that point is on and move to the beginning of the next line.
+    Consecutive calls to this command append each line to the
+    kill-ring."
+  (interactive)
+  (let ((beg (line-beginning-position 1))
+        (end (line-beginning-position 2)))
+    (if (eq last-command 'quick-copy-line)
+        (kill-append (buffer-substring beg end) (< end beg))
+      (kill-new (buffer-substring beg end))))
+  (beginning-of-line 2))
+;; key binding
+(global-set-key "\C-c\M-w" 'quick-copy-line)
+
+;; CUT the current line without selecting it
+(defun quick-cut-line ()
+  "Cut the whole line that point is on. Consecutive calls to this
+command append each line to the kill-ring."
+  (interactive)
+  (let ((beg (line-beginning-position 1))
+        (end (line-beginning-position 2)))
+    (if (eq last-command 'quick-cut-line)
+        (kill-append (buffer-substring beg end) (< end beg))
+      (kill-new (buffer-substring beg end)))
+    (delete-region beg end))
+  (beginning-of-line 1)
+  (setq this-command 'quick-cut-line))
+;; key binding
+(global-set-key "\C-c\M-k" 'quick-cut-line)
