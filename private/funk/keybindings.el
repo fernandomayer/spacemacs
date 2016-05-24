219544c930678d03e21ca7b775a4a30bca9660b0
Fernando Mayer <fernandomayer@gmail.com>
2016-05-24 15:12:48 -0300

    Add shortcuts to change font size

--- private/funk/keybindings.el
+++ private/funk/keybindings.el
@@ -4,3 +4,7 @@
 
 ;; C-TAB move between buffers
 (global-set-key [(control tab)] 'other-window)
+
+;; Change font size
+(define-key global-map (kbd "C-+") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
